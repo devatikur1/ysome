e3f19fa263cf10a75164eb52eb61c0e0ad52188e
Atikur Rahman <fulldevatikur@gmail.com>
2025-11-13 00:49:07 +0600

    Four0Four update

--- src/pages/Four0Four.jsx
+++ src/pages/Four0Four.jsx
@@ -1,11 +1,9 @@
 import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { UiContext } from "../contexts/Ui/UiContext";
 
 export default function Four0Four() {
   const { HomePageOutletWidth, HomePageHeight } = useContext(UiContext);
-  const navigate = useNavigate();
-
   return (
     <div
       style={{
